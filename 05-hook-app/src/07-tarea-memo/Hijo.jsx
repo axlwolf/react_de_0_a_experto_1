078add83de6af13a223f75fb43e7dd73fa43e256
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2023-09-14 14:18:59 -0600

    20. Ejercicio de memo y useCallback

---
+++ 05-hook-app/src/07-tarea-memo/Hijo.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/display-name */
+import { memo } from "react";
+
+export const Hijo = memo(({ numero, incrementar }) => {
+	console.log("  Me volví a generar :(  ");
+
+	return (
+		<button
+			className="btn btn-primary mr-3"
+			onClick={() => incrementar(numero)}
+		>
+			{numero}
+		</button>
+	);
+});
