ad575b1cc942671e99acfe492d04f46560df39da
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2023-12-05 12:36:54 -0600

    41. Calendar app: on Submit event on modal component

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
-import { addHours } from "date-fns";
+import { addHours, differenceInSeconds } from "date-fns";
 
 import Modal from "react-modal";
 import DatePicker, { registerLocale } from "react-datepicker";
@@ -52,6 +52,28 @@ export const CalendarModal = () => {
 		});
 	};
 
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		const difference = differenceInSeconds(formValues.end, formValues.start);
+
+		if (isNaN(difference) || difference <= 0) {
+			console.log("Error on dates");
+			return;
+		}
+
+		if (formValues.title.length <= 0) return;
+
+		console.log(formValues);
+
+		// TODO:
+
+		/*
+			1. Remove screen errors
+			2. Close modal
+		*/
+	};
+
 	return (
 		<Modal
 			isOpen={isOpnen}
@@ -64,7 +86,7 @@ export const CalendarModal = () => {
 		>
 			<h1> Nuevo evento </h1>
 			<hr />
-			<form className="container">
+			<form className="container" onSubmit={onSubmit}>
 				<div className="form-group mb-2">
 					<label>Fecha y hora inicio</label>
 					<DatePicker
